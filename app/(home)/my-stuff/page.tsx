3bd15aeb4d69727a3862258a9bd06aa1d2b4e7e2
Gavin Jakubik <gavito8800@gmail.com>
2024-01-04 17:23:17 -0800

    Another small fix

--- app/(home)/my-stuff/page.tsx
+++ app/(home)/my-stuff/page.tsx
@@ -20,7 +20,7 @@ const HomePage = async () => {
         <Search className="w-full" />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        <RecipeList recipes={recipes} userId={user?.id} />
+        <RecipeList recipes={recipes} user={user} />
       </div>
     </>
   )
