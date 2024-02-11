b70e31c13ddfc22d8953e36ef4f946f6f499a1d9
Michael Lee <michaeldlee3@gmail.com>
2024-02-10 18:58:00 -0800

    wrestling with return values but I think the logic all works

--- rusty-recipi/ingredients/src/main.rs
+++ rusty-recipi/ingredients/src/main.rs
@@ -1,11 +1,28 @@
 // imports 
-use color_eyre::eyre;
-use sqlx::{Error, FromRow, Pool, mysql::{MySql, MySqlPool}, Row, types::chrono::*};
-use std::env;
+use axum::{
+    body::Body,
+    Router,
+    extract::{Extension, Json, Path, State},
+    handler::Handler,
+    http::StatusCode,
+    routing::{get, post, put, delete},
+    response::Response,
+};
+use color_eyre::eyre::{self, Error};
+use eyre::Report;
+use sqlx::{FromRow, Pool, mysql::{MySql, MySqlPool}, Row, types::chrono::*};
+use serde::{Deserialize, Serialize};
+use std::{env, vec};
+use std::sync::Arc;
 use sqlx::*;
 use tokio::*;
+use tracing;
 
-#[derive(FromRow, Debug)]
+struct AppState{
+    pool: Pool<MySql>,
+}
+
+#[derive(FromRow, Debug, Serialize, Deserialize)]
 struct Ingredient {
     id: i32,
     name: String,
@@ -14,34 +31,93 @@ struct Ingredient {
     updated_at: Option<chrono::DateTime<Utc>>,
 }
 
+#[derive(Deserialize, Serialize)]
+struct AddIngredientJson {
+    name: String,
+    calories: i32,
+}
+
+/*
+MAIN
+- Sets up DB connections, registers API routes, and listens for requests
+- Currently supports: 
+    - /ingredient (get, post, put, delete)
+*/
 #[tokio::main]
-async fn main() -> eyre::Result<()>{
+async fn main() {
+    // Set up DB connections
     let database_url = env::var("DATABASE_URL_DEV").expect("DATABASE_URL_DEV must be set");
     let pool = sqlx::MySqlPool::connect(&database_url)
-    .await?;
+    .await;
 
-    add_one::<MySql>(pool.clone()).await;
+    let mut state;
+
+    match pool {
+        Ok(pool) => {
+            println!("Connected to DB");
+            state = AppState { pool };
+        }
+        Err(e) => {
+            println!("Error connecting to DB: {:?}... exiting.", e);
+            return;
+        }
+    }
 
 
-    get_ingredient(&pool, "Egg").await;
-    return Ok(())
+    let router = Router::new()
+        .route(
+            "/ingredient",
+            get(get_ingredient)
+            .with_state(&state)
+        );
 
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    .await
+    .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, router).await.unwrap();
 }
 
-async fn add_one<MySql: sqlx::Database>(pool: sqlx::Pool<sqlx::MySql>) -> bool {
-    let ingredient = Ingredient {
-        id: 2,
-        name: "Egg".to_string(),
-        calories: 72,
-        created_at: None,
-        updated_at: None,
-    };
+// pass in params using State, get request body using JSOn 
+async fn get_ingredient(
+    State(state): State<AppState>,
+    Path(name): Path<String>,
+) -> Json<Ingredient> {
+    let result = sqlx::query(
+        "SELECT * FROM ingredients WHERE name = ?"
+    )
+    .bind(&name)
+    .map(|row: sqlx::mysql::MySqlRow| {
+        Ingredient {
+            id: row.get(0),
+            name: row.get(1),
+            calories: row.get(2),
+            created_at: row.get(3),
+            updated_at: row.get(4),
+        }
+    })
+    .fetch_all(&state.pool).await;
 
-    return add_ingredient(&pool, ingredient, "ingredients_table").await;
-
+    match result {
+        Ok(query_out) => {
+            for ingredient in query_out {
+                println!("Found ingredient: id: {:?}, name: {:?}, calories: {:?}, created_at: {:?}, updated_at: {:?}", 
+                ingredient.id, ingredient.name, ingredient.calories, ingredient.created_at, ingredient.updated_at);
+                println!("Ingredient: {:?}\nJson ingredients: {:?}", ingredient, Json(Some(&ingredient)));
+                return Json(ingredient);
+            }
+        }
+        Err(e) => {
+            println!("Error getting ingredient: {:?}", e);
+            return Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::from("Ingredient not found"))
+                .unwrap();
+        }
+    }
 }
 
-async fn add_ingredient(pool: &sqlx::Pool<MySql>, ingredient: Ingredient, table: &str) -> bool {
+async fn add_ingredient(pool: &sqlx::Pool<MySql>, ingredient: Ingredient) -> bool {
     let res = sqlx::query(
         "INSERT INTO ingredients (
             id,
@@ -62,35 +138,6 @@ async fn add_ingredient(pool: &sqlx::Pool<MySql>, ingredient: Ingredient, table:
         Err(e) => {
             println!("Error inserting ingredient: {:?}", e);
             return false;
-        }
-    }
-}
-
-async fn get_ingredient(pool: &sqlx::Pool<MySql>, name: &str) {
-    let result = sqlx::query(
-        "SELECT * FROM ingredients WHERE name = ?"
-    )
-    .bind(name)
-    .map(|row: sqlx::mysql::MySqlRow| {
-        Ingredient {
-            id: row.get(0),
-            name: row.get(1),
-            calories: row.get(2),
-            created_at: row.get(3),
-            updated_at: row.get(4),
-        }
-    })
-    .fetch_all(pool).await;
-
-    match result {
-        Ok(query_out) => {
-            for ingredient in query_out.iter() {
-                println!("Found ingredient: id: {:?}, name: {:?}, calories: {:?}, created_at: {:?}, updated_at: {:?}", 
-                ingredient.id, ingredient.name, ingredient.calories, ingredient.created_at, ingredient.updated_at);
-            }
-        }
-        Err(e) => {
-            println!("Error getting ingredient: {:?}", e);
         }
     }
 }
