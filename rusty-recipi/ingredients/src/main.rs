72ab6447582a3435ce3aedf4e2d3c2f0a7d364d8
Michael Lee <michaeldlee3@gmail.com>
2024-02-11 13:32:41 -0800

    CRUD for ingredients is done

--- rusty-recipi/ingredients/src/main.rs
+++ rusty-recipi/ingredients/src/main.rs
@@ -1,96 +1,164 @@
-// imports 
-use color_eyre::eyre;
-use sqlx::{Error, FromRow, Pool, mysql::{MySql, MySqlPool}, Row, types::chrono::*};
-use std::env;
+use axum::{
+    body::Body,
+    Router,
+    extract::{Extension, Path, Query, State},
+    handler::Handler,
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post, put, delete},
+    Json,
+    response::Response,
+};
+use color_eyre::eyre::{self, Error};
+use eyre::Report;
+use sqlx::{FromRow, Pool, mysql::{MySql, MySqlPool}, Row, types::chrono::*};
+use serde::{Deserialize, Serialize};
+use std::{env, vec};
 use sqlx::*;
 use tokio::*;
+use tracing;
+use crate::data_access::*;
 
-#[derive(FromRow, Debug)]
-struct Ingredient {
-    id: i32,
-    name: String,
-    calories: i32,
-    created_at: Option<chrono::DateTime<Utc>>,
-    updated_at: Option<chrono::DateTime<Utc>>,
-}
-
+pub mod data_access;
+/*
+MAIN
+- Sets up DB connections, registers API routes, and listens for requests
+- Currently supports: 
+    - /ingredient (get, post, put, delete)
+*/
 #[tokio::main]
-async fn main() -> eyre::Result<()>{
+async fn main() {
+    // Set up DB connections
     let database_url = env::var("DATABASE_URL_DEV").expect("DATABASE_URL_DEV must be set");
-    let pool = sqlx::MySqlPool::connect(&database_url)
-    .await?;
+    let pool = sqlx::MySqlPool::connect(&database_url).await;
 
-    add_one::<MySql>(pool.clone()).await;
-
-
-    get_ingredient(&pool, "Egg").await;
-    return Ok(())
-
-}
-
-async fn add_one<MySql: sqlx::Database>(pool: sqlx::Pool<sqlx::MySql>) -> bool {
-    let ingredient = Ingredient {
-        id: 2,
-        name: "Egg".to_string(),
-        calories: 72,
-        created_at: None,
-        updated_at: None,
+    let pool = match pool {
+        Ok(pool) => pool,
+        Err(e) => {
+            println!("Error connecting to DB: {:?}", e);
+            return;
+        }
     };
 
-    return add_ingredient(&pool, ingredient, "ingredients_table").await;
+    let router = Router::new()
+        .route(
+            "/ingredient",
+            post(add_ingredient_handler)
+            .with_state(pool.clone())
+        )
+        .route(
+            "/ingredient/:id",
+            get(get_ingredient_handler_id)
+            .put(update_ingredient_handler_id)
+            .delete(delete_ingredient_handler_id)
+            .with_state(pool.clone())
+        )
+        .route(
+            "/ingredient/name/:name",
+            get(get_ingredient_handler_name)
+            .delete(delete_ingredient_handler_name)
+            .put(update_ingredient_handler_name)
+            .with_state(pool.clone())
+        );
 
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    .await
+    .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, router).await.unwrap();
 }
 
-async fn add_ingredient(pool: &sqlx::Pool<MySql>, ingredient: Ingredient, table: &str) -> bool {
-    let res = sqlx::query(
-        "INSERT INTO ingredients (
-            id,
-            name,
-            calories)
-        VALUES (?, ?, ?)")
-        .bind(ingredient.id)
-        .bind(ingredient.name)
-        .bind(ingredient.calories)
-        .execute(pool).await;
+async fn get_ingredient_handler_id(
+    Path(id): Path<i32>,
+    State(pool): State<Pool<MySql>>
+) -> impl IntoResponse {
+    let result = get_ingredient_id(id, pool).await;
 
-    println!("Result: {:?}", res);
+    for ingredient in result {
+        println!("Ingredient: {:?}", ingredient);
+        return (StatusCode::OK, Json(ingredient)).into_response();
+    }
+    (StatusCode::NOT_FOUND, "No ingredient found").into_response()
+}
 
-    match res {
-        Ok(_) => {
-            return true;
-        }
-        Err(e) => {
-            println!("Error inserting ingredient: {:?}", e);
-            return false;
-        }
+async fn get_ingredient_handler_name(
+    Path(name): Path<String>,
+    State(pool): State<Pool<MySql>>
+) -> impl IntoResponse {
+    let result = get_ingredient_name(name, pool).await;
+
+    for ingredient in result {
+        println!("Ingredient: {:?}", ingredient);
+        return (StatusCode::OK, Json(ingredient)).into_response();
+    }
+    (StatusCode::NOT_FOUND, "No ingredient found").into_response()
+}
+
+async fn add_ingredient_handler(
+    State(pool): State<Pool<MySql>>,
+    Json(ingredient): Json<Ingredient>
+) -> impl IntoResponse {
+    let result = add_ingredient(pool, ingredient).await;
+
+    if result {
+        (StatusCode::CREATED, "Ingredient added").into_response()
+    } else {
+        (StatusCode::INTERNAL_SERVER_ERROR, "Error adding ingredient").into_response()
     }
 }
 
-async fn get_ingredient(pool: &sqlx::Pool<MySql>, name: &str) {
-    let result = sqlx::query(
-        "SELECT * FROM ingredients WHERE name = ?"
-    )
-    .bind(name)
-    .map(|row: sqlx::mysql::MySqlRow| {
-        Ingredient {
-            id: row.get(0),
-            name: row.get(1),
-            calories: row.get(2),
-            created_at: row.get(3),
-            updated_at: row.get(4),
-        }
-    })
-    .fetch_all(pool).await;
+async fn update_ingredient_handler_name(
+    State(pool): State<Pool<MySql>>,
+    Path(name): Path<String>,
+    Json(ingredient): Json<Ingredient>
+) -> impl IntoResponse {
+    let result = update_ingredient_name(pool, ingredient, name).await;
 
-    match result {
-        Ok(query_out) => {
-            for ingredient in query_out.iter() {
-                println!("Found ingredient: id: {:?}, name: {:?}, calories: {:?}, created_at: {:?}, updated_at: {:?}", 
-                ingredient.id, ingredient.name, ingredient.calories, ingredient.created_at, ingredient.updated_at);
-            }
-        }
-        Err(e) => {
-            println!("Error getting ingredient: {:?}", e);
-        }
+    if result {
+        (StatusCode::OK, "Ingredient updated").into_response()
+    } else {
+        (StatusCode::INTERNAL_SERVER_ERROR, "Error updating ingredient").into_response()
+    }
+}
+
+async fn update_ingredient_handler_id(
+    State(pool): State<Pool<MySql>>,
+    Path(id): Path<i32>,
+    Json(ingredient): Json<Ingredient>
+) -> impl IntoResponse {
+    let result = update_ingredient_id(pool, ingredient, id).await;
+
+    if result {
+        (StatusCode::OK, "Ingredient updated").into_response()
+    } else {
+        (StatusCode::INTERNAL_SERVER_ERROR, "Error updating ingredient").into_response()
+    }
+}
+
+async fn delete_ingredient_handler_id(
+    Path(id): Path<i32>,
+    State(pool): State<Pool<MySql>>
+) -> impl IntoResponse
+{
+    let result = delete_ingredient_id(id, pool).await;
+
+    if result {
+        (StatusCode::OK, "Ingredient deleted").into_response()
+    } else {
+        (StatusCode::INTERNAL_SERVER_ERROR, "Error deleting ingredient").into_response()
+    }
+}
+
+async fn delete_ingredient_handler_name(
+    Path(name): Path<String>,
+    State(pool): State<Pool<MySql>>
+) -> impl IntoResponse
+{
+    let result = delete_ingredient_name(name, pool).await;
+
+    if result {
+        (StatusCode::OK, "Ingredient deleted").into_response()
+    } else {
+        (StatusCode::INTERNAL_SERVER_ERROR, "Error deleting ingredient").into_response()
     }
 }
