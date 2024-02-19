caacd333927f61b7b4f60fab41338860682e41d1
Michael Lee <michaeldlee3@gmail.com>
2024-02-19 09:42:46 -0800

    I think I cooked?

--- rusty-recipi/ingredients/src/ingredient.rs
+++ rusty-recipi/ingredients/src/ingredient.rs
@@ -5,117 +5,68 @@ use std::{env, vec, path::Path as FilePath, fs::File};
 use std::io::BufReader;
 use std::io::BufRead;
 use std::fmt::Display;
+use sqlx::{FromRow, mysql::{MySql}, Row, types::chrono::*};
+
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Ingredient2 {
     id: i64,
-    name: String,
+    description: String,
     calories: f32, // calories per 100g
     protein: f32, // protein per 100g
     fat: f32, // fat per 100g
     carbs: f32, // carbs per 100g
-    portions: Vec<IngredientPortion>,
 }
 
 impl Ingredient2 {
-    pub fn new(id: i64, name: &str, calories: f32, protein: f32, fat: f32, carbs: f32, portions: Vec<IngredientPortion>) -> Ingredient2 {
+    pub fn new(id: i64, description: &str, calories: f32, protein: f32, fat: f32, carbs: f32) -> Ingredient2 {
         Ingredient2 {
             id,
-            name: name.to_string(),
+            description: description.to_string(),
             calories,
             protein,
             fat,
             carbs,
-            portions
         }
     }
 }
 
 impl fmt::Display for Ingredient2 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // let portions = self.portions.iter().map(|Some(x)| x.to_string()).collect::<Vec<String>>().join(", ");
-        let mut portions = String::from("");
-
-        write!(f, "Ingredient: id: {}, name: {}, calories/100g: {}, protein/100g: {}, fat/100g: {}, carbs/100g: {}, portions: {:?}", self.id, self.name, self.calories, self.protein, self.fat, self.carbs, self.portions)
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct IngredientPortion {
-    measure_unit: String,
-    measure_value: i32,
-    gram_weight: f64,
-}
-
-impl fmt::Display for IngredientPortion {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Portion: measure_unit: {}, measure_value: {}, gram_weight: {}", self.measure_unit, self.measure_value, self.gram_weight)
-    }
-}
-
-fn raw_portion_to_ingredient_portion(raw_portion: &Option<FoodPortion>) -> IngredientPortion {
-    match raw_portion {
-        Some(p) => IngredientPortion {
-            measure_unit: p.measureUnit.name.to_string(),
-            measure_value: p.value as i32,
-            gram_weight: p.gramWeight as f64,
-        },
-        None => IngredientPortion {
-            measure_unit: "".to_string(),
-            measure_value: 0,
-            gram_weight: 0.0,
-        }
+        write!(f, "Ingredient: id: {}, name: {}, calories/100g: {}, protein/100g: {}, fat/100g: {}, carbs/100g: {}", self.id, self.description, self.calories, self.protein, self.fat, self.carbs)
     }
 }
 
 // ingest from each dataset
 pub fn ingest_foundation_foods() {
     let mut ings: Vec<Ingredient2> = Vec::new();
-    let path = FilePath::new("/Users/michaellee/recipi/.data/foundation_foods.json");
+    let path = FilePath::new("C:/Users/micha/recipi/.data/foundation_foods.json");
     let f = File::open(path).unwrap();
     let mut reader = BufReader::new(f);
     let mut lines = reader.lines();
     lines.next();
     
+    let mut num = 0; 
+
     for l in lines {
         let mut line = l.unwrap();
         line = line.trim_matches(',').to_string();
         let result: Result<Food, serde_json::Error> = serde_json::from_str(&line);
         match result {
             Ok(raw_food) => {
-                let description = match raw_food.description {
-                    Some(d) => d,
-                    None => continue,
-                };
-
-                let protein_nutrient = raw_food.foodNutrients.iter().find(|x| x.nutrient.id == 1003);
-                let protein = match protein_nutrient {
-                    Some(p) => p.amount.unwrap_or(0.0),
-                    None => 0.0,
-                };
-
-                let fat_nutrient = raw_food.foodNutrients.iter().find(|x| x.nutrient.id == 1004);
-                let fat = match fat_nutrient {
-                    Some(f) => f.amount.unwrap_or(0.0),
-                    None => 0.0,
-                };
-
-                let carbs_nutrient = raw_food.foodNutrients.iter().find(|x| x.nutrient.id == 1005);
-                let carbs = match carbs_nutrient {
-                    Some(c) => c.amount.unwrap_or(0.0),
-                    None => 0.0,
-                };
-                
-                let calories_nutrient = raw_food.foodNutrients.iter().find(|x| x.nutrient.id == 1008);
-                let calories = match calories_nutrient {
-                    Some(c) => c.amount.unwrap_or(0.0),
-                    None => 0.0,
-                };
-
-                let portions = raw_food.foodPortions.iter().map(|x| raw_portion_to_ingredient_portion(x)).collect::<Vec<IngredientPortion>>();
-
-                let ing = Ingredient2::new(raw_food.fdcId.into(), &description, calories, protein, fat, carbs, portions);
-                ings.push(ing);
+                let ingredient = ingredient_from_raw_food(raw_food);
+                match ingredient {
+                    Some(i) => {
+                        println!("Ingredient: {}", i);
+                        ings.push(i);
+                    }
+                    None => {
+                        println!("Error: Could not create ingredient from raw food");
+                        continue;
+                    }
+                }
+                num += 1;
+                break;
             }
             Err(e) => {
                 println!("Error: {:?}", e);
@@ -124,61 +75,37 @@ pub fn ingest_foundation_foods() {
         }
     }
 
-    println!("Number of ings: {}", ings.len());
-    for ing in ings {
-        println!("{}", ing);
-    }
+    println!("Number of foundation foods: {}", num);
 }
 
-fn ingest_sr_legacy_foods() {
-    let mut ings: Vec<Ingredient2> = Vec::new();
-    let path = FilePath::new("/Users/michaellee/recipi/.data/sr_legacy_foods.json");
+pub fn ingest_sr_legacy_foods() {
+    let path = FilePath::new("C:/Users/micha/recipi/.data/sr_legacy_foods.json");
     let f = File::open(path).unwrap();
     let mut reader = BufReader::new(f);
     let mut lines = reader.lines();
     lines.next();
     
-    // for l in lines {
-    for i in 0..1 {
-        let l = lines.nth(i);
+    let mut num = 0;
+
+    for l in lines {
         let mut line = l.unwrap();
         line = line.trim_matches(',').to_string();
         let result: Result<Food, serde_json::Error> = serde_json::from_str(&line);
         match result {
             Ok(raw_food) => {
-                let description = match raw_food.description {
-                    Some(d) => d,
-                    None => continue,
-                };
-
-                let protein_nutrient = raw_food.foodNutrients.iter().find(|x| x.nutrient.id == 1003);
-                let protein = match protein_nutrient {
-                    Some(p) => p.amount.unwrap_or(0.0),
-                    None => 0.0,
-                };
-
-                let fat_nutrient = raw_food.foodNutrients.iter().find(|x| x.nutrient.id == 1004);
-                let fat = match fat_nutrient {
-                    Some(f) => f.amount.unwrap_or(0.0),
-                    None => 0.0,
-                };
-
-                let carbs_nutrient = raw_food.foodNutrients.iter().find(|x| x.nutrient.id == 1005);
-                let carbs = match carbs_nutrient {
-                    Some(c) => c.amount.unwrap_or(0.0),
-                    None => 0.0,
-                };
-                
-                let calories_nutrient = raw_food.foodNutrients.iter().find(|x| x.nutrient.id == 1008);
-                let calories = match calories_nutrient {
-                    Some(c) => c.amount.unwrap_or(0.0),
-                    None => 0.0,
-                };
-
-                let portions = raw_food.foodPortions.iter().map(|x| raw_portion_to_ingredient_portion(x)).collect::<Vec<IngredientPortion>>();
-
-                let ing = Ingredient2::new(raw_food.fdcId.into(), &description, calories, protein, fat, carbs, portions);
-                ings.push(ing);
+                let ingredient = ingredient_from_raw_food(raw_food);
+                match ingredient {
+                    Some(i) => {
+                        println!("Ingredient: {}", i);
+                        ings.push(i);
+                    }
+                    None => {
+                        println!("Error: Could not create ingredient from raw food");
+                        continue;
+                    }
+                }
+                num += 1;
+                break;
             }
             Err(e) => {
                 println!("Error: {:?}", e);
@@ -187,12 +114,128 @@ fn ingest_sr_legacy_foods() {
         }
     }
 
-    println!("Number of ings: {}", ings.len());
-    for ing in ings {
-        println!("{}", ing);
-    }
+    println!("Number of sr legacy foods: {}", num);
 }
 
+pub fn ingest_fndds_foods() {
+    let path = FilePath::new("C:/Users/micha/recipi/.data/fndds_foods.json");
+    let f = File::open(path).unwrap();
+    let mut reader = BufReader::new(f);
+    let mut lines = reader.lines();
+    lines.next();
+    
+    let mut num = 0;
+
+    for l in lines {
+        let mut line = l.unwrap();
+        line = line.trim_matches(',').to_string();
+        let result: Result<Food, serde_json::Error> = serde_json::from_str(&line);
+        match result {
+            Ok(raw_food) => {
+                let ingredient = ingredient_from_raw_food(raw_food);
+                match ingredient {
+                    Some(i) => {
+                        println!("Ingredient: {}", i);
+                        ings.push(i);
+                    }
+                    None => {
+                        println!("Error: Could not create ingredient from raw food");
+                        continue;
+                    }
+                }
+                num += 1;
+                break;
+            }
+            Err(e) => {
+                println!("Error: {:?}", e);
+                continue;
+            }
+        }
+    }
+
+    println!("Number of fndds foods: {}", num);
+}
+
+pub fn ingest_branded_foods() {
+    let mut ings: Vec<Ingredient2> = Vec::new();
+    let path = FilePath::new("C:/Users/micha/recipi/.data/branded_foods.json");
+    let f = File::open(path).unwrap();
+    let mut reader = BufReader::new(f);
+    let mut lines = reader.lines();
+    lines.next();
+    
+    let mut num = 0;
+
+    for l in lines {
+        let mut line = l.unwrap();
+        line = line.trim_matches(',').to_string();
+        let result: Result<Food, serde_json::Error> = serde_json::from_str(&line);
+        match result {
+            Ok(raw_food) => {
+                let ingredient = ingredient_from_raw_food(raw_food);
+                match ingredient {
+                    Some(i) => {
+                        println!("Ingredient: {}", i);
+                        ings.push(i);
+                    }
+                    None => {
+                        println!("Error: Could not create ingredient from raw food");
+                        continue;
+                    }
+                }
+                num += 1;
+                break;
+            }
+            Err(e) => {
+                println!("Error: {:?}", e);
+                continue;
+            }
+        }
+    }
+
+    println!("Number of branded foods: {}", num);
+}
+
+fn ingredient_from_raw_food(raw_food: Food) -> Option<Ingredient2>
+{
+    let description = match raw_food.description {
+        Some(d) => d,
+        None => return None,
+    };
+
+    let protein_nutrient = raw_food.foodNutrients.iter().find(|x| x.nutrient.id == 1003);
+    let protein = match protein_nutrient {
+        Some(p) => p.amount.unwrap_or(0.0),
+        None => 0.0,
+    };
+
+    let fat_nutrient = raw_food.foodNutrients.iter().find(|x| x.nutrient.id == 1004);
+    let fat = match fat_nutrient {
+        Some(f) => f.amount.unwrap_or(0.0),
+        None => 0.0,
+    };
+
+    let carbs_nutrient = raw_food.foodNutrients.iter().find(|x| x.nutrient.id == 1005);
+    let carbs = match carbs_nutrient {
+        Some(c) => c.amount.unwrap_or(0.0),
+        None => 0.0,
+    };
+    
+    let calories_nutrient = raw_food.foodNutrients.iter().find(|x| x.nutrient.id == 1008);
+    let calories = match calories_nutrient {
+        Some(c) => c.amount.unwrap_or(0.0),
+        None => 0.0,
+    };
+
+    Some(Ingredient2::new(raw_food.fdcId.into(), &description, calories, protein, fat, carbs))
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FoodAttribute {
+    id: Option<u32>,
+    name: Option<String>,
+    description: Option<String>,
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct FoodNutrientSource {
@@ -213,8 +256,8 @@ struct Nutrient {
     id: u32,
     number: String,
     name: String,
-    rank: u32,
-    unitName: String,
+    rank: Option<u32>,
+    unitName: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -223,7 +266,7 @@ struct FoodNutrient {
     type_: String,
     id: u32,
     nutrient: Nutrient,
-    foodNutrientDerivation: FoodNutrientDerivation,
+    foodNutrientDerivation: Option<FoodNutrientDerivation>,
     max: Option<f32>,
     min: Option<f32>,
     median: Option<f32>,
@@ -233,20 +276,20 @@ struct FoodNutrient {
 #[derive(Debug, Serialize, Deserialize)]
 struct MeasureUnit {
     id: u32,
-    name: String,
-    abbreviation: String,
+    name: Option<String>,
+    abbreviation: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct FoodPortion {
     id: u32,
-    value: f32,
-    measureUnit: MeasureUnit,
+    value: Option<f32>,
+    measureUnit: Option<MeasureUnit>,
     modifier: Option<String>,
-    gramWeight: f32,
-    sequenceNumber: u32,
-    minYearAcquired: u32,
-    amount: f32,
+    gramWeight: Option<f32>,
+    sequenceNumber: Option<u32>,
+    minYearAcquired: Option<u32>,
+    amount: Option<f32>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -256,25 +299,25 @@ struct FoodCategory {
 
 #[derive(Debug, Serialize, Deserialize)]
 struct InputFood {
-    foodClass: String,
+    foodClass: Option<String>,
     description: Option<String>,
-    dataType: String,
-    foodCategory: FoodCategory,
+    dataType: Option<String>,
+    foodCategory: Option<FoodCategory>,
     fdcId: u32,
-    publicationDate: String,
+    publicationDate: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct InputFoods {
-    id: u32,
-    foodDescription: String,
-    inputFood: InputFood,
+    id: Option<u32>,
+    foodDescription: Option<String>,
+    inputFood: Option<InputFood>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct NutrientConversionFactor {
     #[serde(rename = "type")]
-    type_: String,
+    type_: Option<String>,
     value: Option<f32>,
     proteinValue: Option<f32>,
     fatValue: Option<f32>,
@@ -283,14 +326,17 @@ struct NutrientConversionFactor {
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Food {
-    foodClass: String,
+    foodClass: Option<String>,
     description: Option<String>,
     foodNutrients: Vec<FoodNutrient>,
-    foodAttributes: Vec<String>,
-    nutrientConversionFactors: Vec<NutrientConversionFactor>,
-    ndbNumber: u32,
-    foodCategory: FoodCategory,
+    foodAttributes: Vec<Option<FoodAttribute>>,
+    nutrientConversionFactors: Option<Vec<Option<NutrientConversionFactor>>>,
+    ndbNumber: Option<u32>,
+    foodCategory: Option<FoodCategory>,
     fdcId: u32,
-    foodPortions: Vec<Option<FoodPortion>>,
-    inputFoods: Vec<InputFoods>,
+    foodPortions: Option<Vec<Option<FoodPortion>>>,
+    inputFoods: Option<Vec<InputFoods>>,
+    #[serde(rename = "fdcId")]
+    fdc_id__: Option<u32>,
+    dataType: Option<String>,
 }
--- rusty-recipi/ingredients/src/main.rs
+++ rusty-recipi/ingredients/src/main.rs
@@ -31,10 +31,14 @@ MAIN
 */
 #[tokio::main]
 async fn main() {
-    // let path = FilePath::new("/Users/michaellee/recipi/.data/branded_foods.json");
-    // let path = FilePath::new("/Users/michaellee/recipi/.data/sr_legacy_foods.json");
-    // let path = FilePath::new("/Users/michaellee/recipi/.data/fndds_foods.json");
+    println!("-------- FOUNDATION FOODS --------");
+    ingredient::ingest_foundation_foods(); // works lets go
+    println!("-------- SR LEGACY FOODS --------");
     ingredient::ingest_sr_legacy_foods();
+    println!("-------- FNDDS FOODS --------");
+    ingredient::ingest_fndds_foods();
+    println!("-------- BRANDED FOODS --------");
+    ingredient::ingest_branded_foods();
 
     return;
 
@@ -49,125 +53,4 @@ async fn main() {
             return;
         }
     };
-
-    let router = Router::new()
-        .route(
-            "/ingredient",
-            post(add_ingredient_handler)
-            .with_state(pool.clone())
-        )
-        .route(
-            "/ingredient/:id",
-            get(get_ingredient_handler_id)
-            .put(update_ingredient_handler_id)
-            .delete(delete_ingredient_handler_id)
-            .with_state(pool.clone())
-        )
-        .route(
-            "/ingredient/name/:name",
-            get(get_ingredient_handler_name)
-            .delete(delete_ingredient_handler_name)
-            .put(update_ingredient_handler_name)
-            .with_state(pool.clone())
-        );
-
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-    .await
-    .unwrap();
-    axum::serve(listener, router).await.unwrap();
-}
-
-async fn get_ingredient_handler_id(
-    Path(id): Path<i32>,
-    State(pool): State<Pool<MySql>>
-) -> impl IntoResponse {
-    let result = get_ingredient_id(id, pool).await;
-
-    for ingredient in result {
-        println!("Ingredient: {:?}", ingredient);
-        return (StatusCode::OK, Json(ingredient)).into_response();
-    }
-    (StatusCode::NOT_FOUND, "No ingredient found").into_response()
-}
-
-async fn get_ingredient_handler_name(
-    Path(name): Path<String>,
-    State(pool): State<Pool<MySql>>
-) -> impl IntoResponse {
-    let result = get_ingredient_name(name, pool).await;
-
-    for ingredient in result {
-        println!("Ingredient: {:?}", ingredient);
-        return (StatusCode::OK, Json(ingredient)).into_response();
-    }
-    (StatusCode::NOT_FOUND, "No ingredient found").into_response()
-}
-
-async fn add_ingredient_handler(
-    State(pool): State<Pool<MySql>>,
-    Json(ingredient): Json<Ingredient>
-) -> impl IntoResponse {
-    let result = add_ingredient(pool, ingredient).await;
-
-    if result {
-        (StatusCode::CREATED, "Ingredient added").into_response()
-    } else {
-        (StatusCode::INTERNAL_SERVER_ERROR, "Error adding ingredient").into_response()
-    }
-}
-
-async fn update_ingredient_handler_name(
-    State(pool): State<Pool<MySql>>,
-    Path(name): Path<String>,
-    Json(ingredient): Json<Ingredient>
-) -> impl IntoResponse {
-    let result = update_ingredient_name(pool, ingredient, name).await;
-
-    if result {
-        (StatusCode::OK, "Ingredient updated").into_response()
-    } else {
-        (StatusCode::INTERNAL_SERVER_ERROR, "Error updating ingredient").into_response()
-    }
-}
-
-async fn update_ingredient_handler_id(
-    State(pool): State<Pool<MySql>>,
-    Path(id): Path<i32>,
-    Json(ingredient): Json<Ingredient>
-) -> impl IntoResponse {
-    let result = update_ingredient_id(pool, ingredient, id).await;
-
-    if result {
-        (StatusCode::OK, "Ingredient updated").into_response()
-    } else {
-        (StatusCode::INTERNAL_SERVER_ERROR, "Error updating ingredient").into_response()
-    }
-}
-
-async fn delete_ingredient_handler_id(
-    Path(id): Path<i32>,
-    State(pool): State<Pool<MySql>>
-) -> impl IntoResponse
-{
-    let result = delete_ingredient_id(id, pool).await;
-
-    if result {
-        (StatusCode::OK, "Ingredient deleted").into_response()
-    } else {
-        (StatusCode::INTERNAL_SERVER_ERROR, "Error deleting ingredient").into_response()
-    }
-}
-
-async fn delete_ingredient_handler_name(
-    Path(name): Path<String>,
-    State(pool): State<Pool<MySql>>
-) -> impl IntoResponse
-{
-    let result = delete_ingredient_name(name, pool).await;
-
-    if result {
-        (StatusCode::OK, "Ingredient deleted").into_response()
-    } else {
-        (StatusCode::INTERNAL_SERVER_ERROR, "Error deleting ingredient").into_response()
-    }
 }
