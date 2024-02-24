42dfae124e365195a10e9a4d2dcc3608240a4e1d
Michael Lee <michaeldlee3@gmail.com>
2024-02-24 10:12:18 -0800

    dummy commit

--- rusty-recipi/ingredients/src/main.rs
+++ rusty-recipi/ingredients/src/main.rs
@@ -37,20 +37,26 @@ async fn main() {
             return;
         }
     };
-/*
+    
+    // ingredient deletion
+    /*
     data_access::delete_all_ingredients(pool).await;
     println!("done deleting ingredients");
-    return;*/
+    return;
+    */
 
     //println!("-------- FOUNDATION FOODS --------");
     //ingredient::ingest_foundation_foods(&pool).await; // works lets go
+    
     println!("-------- SR LEGACY FOODS --------");
     ingredient::ingest_sr_legacy_foods(&pool).await;
     
-    /*println!("-------- FNDDS FOODS --------");
+    /*
+    println!("-------- FNDDS FOODS --------");
     ingredient::ingest_fndds_foods(&pool).await;
     println!("-------- BRANDED FOODS --------");
-    ingredient::ingest_branded_foods(&pool).await;*/
+    ingredient::ingest_branded_foods(&pool).await;
+    */
 
     return; 
 
