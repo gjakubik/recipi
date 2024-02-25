c64aea11c2b9c9319466e8ba1ae2075cd1e0dffb
Gavin Jakubik <gavito8800@gmail.com>
2024-02-25 01:18:41 -0500

    ts fix

--- recipi-next/app/(home)/my-stuff/page.tsx
+++ recipi-next/app/(home)/my-stuff/page.tsx
@@ -18,7 +18,7 @@ const HomePage = async () => {
   return (
     <>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-        <RecipeList initialMenus={initialMenus} recipes={recipes} user={user} />
+        <RecipeList initialMenus={initialMenus} recipes={recipes} />
       </div>
     </>
   )
