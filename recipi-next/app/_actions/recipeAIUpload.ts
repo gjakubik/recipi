2a9858e25b5e575f9281df4ff4c0f7c14502a665
Gavin Jakubik <gavito8800@gmail.com>
2024-02-29 16:49:45 -0500

    Gj/url upload (#54)
    * need to fix rate limiting
    
    * url upload complete

--- recipi-next/app/_actions/recipeAIUpload.ts
+++ recipi-next/app/_actions/recipeAIUpload.ts
@@ -3,38 +3,11 @@
 import { getCurrentUser } from '@/lib/session'
 import OpenAI from 'openai'
 import { UploadFileResponse } from 'uploadthing/client'
-
-const createRecipeGenerationPrompt = (isText: boolean) => `I have ${
-  isText ? 'text' : 'an image'
-} of a recipe and the following data type. Parse what is in the ${
-  isText ? 'text' : 'image'
-} and then transfer the contents into the following data type. 
-    Please be very careful to be specific about the measurements, as you often mistake 4 for 2s in denominators. There will be some fields such as prep time and cooking time that will need to be inferred by you. The amount of servings will also need to be inferred. Even if you are not sure the amount of servings, make a concrete estimate.
-    If you are unable to make a concrete estimate, use the integer 0. Make sure that any amounts are json strings, and that any amounts use fractions instead of decimals. Fractions should be in the form of "1 1/2", with no other special characters used than /
-    Be precise in naming ingredients, and put any extra information related to it in the note field. If there is no note, dont define the field. 
-
-    Generate a JSON object that fits this type
-    Unit may be something different than these, but should be a full word, not an abbreviation
-    type Unit = 'teaspoon' | 'tbs' | 'cup' | 'oz' | 'lb' | 'ml' | 'l' | 'g' | 'kg' | 'whole' | 'round' |'package' | 'clove';
-    type Time = 'hhh:mm:ss'
-    type Amount = 'number' | 'number number/number'
-
-    interface RecipeForm {
-      title: string;
-      description: string;
-      preparationTime: Time;
-      cookingTime: Time;
-      servings: string;
-      difficultyLevel: string;
-      instructions: string[];
-      ingredients?: {
-        amount: Amount;
-        unit: Unit;
-        name: string;
-        note?: string;
-      }[];
-    }
-    Refrain from leaving any comments at all, and output only JSON. Again, this response should start with \`\`\`json and end with \`\`\`, meaning that it is only the json block representing the recipe`
+import {
+  TEXT_PROMPT,
+  IMAGE_PROMPT,
+  URL_PROMPT,
+} from '@/lib/constants/AIPrompts'
 
 const ai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -48,7 +21,7 @@ export const recipeAIUploadText = async (inputText: string) => {
 
   //Creat a request to my open ai assistant to generate recipe json from the input text
   const prompt = `
-    ${createRecipeGenerationPrompt(true)}
+    ${TEXT_PROMPT}
     Recipe Text: ${inputText}
   `
 
@@ -60,6 +33,58 @@ export const recipeAIUploadText = async (inputText: string) => {
   const dirtyResponse = gptResponse.choices[0].message.content
 
   // strip all text inside ```json and ```
+  const recipeJSON = dirtyResponse?.replace(/```json/g, '').replace(/```/g, '')
+
+  console.log(recipeJSON)
+
+  return recipeJSON
+}
+
+export const recipeAIUploadUrl = async (url: string) => {
+  const user = getCurrentUser()
+  if (!user) {
+    throw new Error('No user found, login to use this feature')
+  }
+
+  // Get the html content of the url
+  const htmlResponse = await fetch(url)
+  const html = await htmlResponse.text()
+
+  if (!html) throw new Error('No html found at url')
+
+  // Try to extract the body of the html
+  const body = html.match(/<body[^>]*>((.|[\n\r])*)<\/body>/)
+
+  if (!body) throw new Error('No body found in html')
+
+  const cleanedBody = body[0]
+    .replace(/<script[\s\S]*?<\/script>/g, '')
+    .replace(/<style[\s\S]*?<\/style>/g, '')
+    .replace(/<svg[\s\S]*?<\/svg>/g, '')
+    .replace(/<img[\s\S]*?>/g, '')
+    .replace(/<form[\s\S]*?<\/form>/g, '')
+    .replace(/<a[\s\S]*?<\/a>/g, '')
+    .replace(/<[^>]*>/g, '')
+
+  console.log(cleanedBody)
+
+  const prompt = `
+    ${URL_PROMPT}
+    HTML: ${cleanedBody}
+  `
+  // Convert the html to text using your AI model
+  const htmlTextResponse = await ai.chat.completions.create({
+    model: 'gpt-3.5-turbo-0125',
+    messages: [
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+  })
+
+  const dirtyResponse = htmlTextResponse.choices[0].message.content
+  // Process the html text to JSON, similar to how it's done in recipeAIUploadText
   const recipeJSON = dirtyResponse?.replace(/```json/g, '').replace(/```/g, '')
 
   console.log(recipeJSON)
@@ -80,7 +105,7 @@ export const recipeAIUploadImage = async (image: UploadFileResponse) => {
       {
         role: 'user',
         content: [
-          { type: 'text', text: createRecipeGenerationPrompt(false) },
+          { type: 'text', text: IMAGE_PROMPT },
           {
             type: 'image_url',
             image_url: {
